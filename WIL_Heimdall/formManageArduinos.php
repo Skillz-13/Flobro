a38f88305cc4b0238c9b4cf6e1ed6fbc56c1d982
Heliosett <42584644+Heliosett@users.noreply.github.com>
2019-10-23 14:17:59 +0200

    Add files via upload

--- WIL_Heimdall/formManageArduinos.php
+++ WIL_Heimdall/formManageArduinos.php
@@ -4,44 +4,39 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-<title>Assign Sales Representative Form</title>
+<title>Manage Arduino Form</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/main.css" type="text/css">
-<link rel="stylesheet" href="css/formAssignSalesRep.css" type="text/css">
+<link rel="stylesheet" href="css/formManageArduinos.css" type="text/css">
 </head>
 <body style="background-image: url(images/woodtexture.jpg);">
 	<div class="container">
 		<!--NAVIGATION BAR PANEL-->
 		<div class="panelNavBar">
 			<img id="imgLogo" src="images/logoBoston.png">
-			<div id="lblCurrentPage"><p><b>Assign Sales Rep</b></p></div>
-				<a name="btnHome" id="btnHome" class="btn" href="dashboardAdminSupport.php"><p>Home</p></a>
-				<a name="btnManageLocations" id="btnManageLocations" class="btn" href="formManageLocations.php"><p>Manage Locations</p></a>
-				<a name="btnManageCompanies" id="btnManageCompanies" class="btn" href="formManageCompanies.php"><p>Manage Companies</p></a>
-				<a name="btnManageUsers" id="btnManageUsers" class="btn" href="adminManageUsers.php"><p>Manage Users</p></a>
-				<a name="btnReports" id="btnReports" class="btn" href=""><p>Reports</p></a>
+			<div id="lblCurrentPage"><p><b>Manage Arduinos</b></p></div>
+				<a name="btnHome" id="btnHome" class="btn" href="dashboardAdminHardware.php"><p>Home</p></a>
+				<a name="btnManageFlowmeters" id="btnManageFlowmeters" class="btn" href="formManageFlowmeters.php"><p>Manage Flowmeters</p></a>
+				<a name="btnAssignArduino" id="btnAssignArduino" class="btn" href="formAssignArduinos.php"><p>Assign Arduinos</p></a>
+				<a id="-" class="btn" href=""><p>-</p></a>
 				<a name="btnLogout" id="btnLogout" class="btn" href="logout.php"><p>Log Out</p></a>
 		</div>
 		<!--HEADER PANEL-->
         <div class="panelHeader">
 			<div id="lblWelcomeMessage">
-			<p>Support Admin Dashboard</p>
+			<p>Hardware Admin Dashboard</p>
 			<p>You are logged in as -Name- -Surname-</p>
 			</div>
 		</div>
 		<!--MAIN PANEL-->
 		<div class="panelMain">
-			<div class="panelMainUserDisplay">
+			<div class="panelMainArduinoTable">
 			<form>
-				<label for="txtBoxSalesReps" class="col-form-label"><u><h5>Users:</h5></u></label>
-				<select name="ddlUserType" id="ddlUserType" class="form-control">
-					<option value="Sales Representatives">Sales Representative</option>
-					<option value="Bar Manager">Bar Manager</option>
-				</select>
-				<table class="tblUsers" name ="tblUsers">
+				<label for="txtBoxSalesReps" class="col-form-label"><u><h5>Registered Arduinos:</h5></u></label>
+				<table class="tblArduino" name ="tblArduino">
 				<tr>
 					<td>
-						<table id = "tblUsersHeader" class="tblUsers" name = "tblUsersHeader">
+						<table id = "tblArduinoHeader" class="tblArduino" name = "tblArduinoHeader">
 							<tr>
 								<th>Name</th>
 								<th>Surname</th>
@@ -52,8 +47,8 @@
 				<tr>
 					<td>
 						<!--CONTENT OF TABLE-->
-						<div id="divTableUsersContent" style="width:100%; height:340px; overflow:auto;">
-							<table name="tblUsersContent" id = "tblUsersContent" class="tblUsers">
+						<div id="divTableArduinoContent" style="width:100%; height:340px; overflow:auto;">
+							<table name="tblArduinoContent" id = "tblArduinoContent" class="tblArduino">
 								<tr>
 									<td>Name</td>
 									<td>Surname</td>
@@ -66,7 +61,7 @@
 			</form>
 			</div>
 			
-			<div class="panelMainLocationDisplay">
+			<div class="panelMainFormElements">
 			<form>
 			<label for="txtBoxLocation" class="col-form-label"><u><h5>All Locations:</h5></u></label>
 			<table class="tblLocation" name ="tblLocation">
@@ -98,7 +93,7 @@
 			<button name="btnAddLocation" class="btn btnAdd"><p>Add</p></button>
 			</div>
 			
-			<div class="panelMainAssLocationDisplay">
+			<div class="panelMainButtons">
 			<form>
 			<label for="txtBoxAssLocation" class="col-form-label"><u><h5>Assigned Locations:</h5></u></label>
 			<table class="tblAssLocation" name ="tblAssLocation">
